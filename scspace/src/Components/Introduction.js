dda564d84edf941cfeea504e0eb486a7c5dee068
choisw0823 <choisw0823@naver.com>
2022-07-15 02:19:02 +0900

    introduction 페이지 수정중

--- scspace/src/Components/Introduction.js
+++ scspace/src/Components/Introduction.js
@@ -7,9 +7,9 @@ class Introduction extends Component{
 
         this.state = {
           info: [
-            {which:'소개', text:'1', clicked:true},
-            {which:'사업소개', text:'2', clicked:false},
-            {which:'회칙', text:'3', clicked:false}
+            {which:'소개', text:'1ddd', clicked:true},
+            {which:'사업소개', text:'2sdfs', clicked:false},
+            {which:'회칙', text:'3sdss', clicked:false}
           ]
         }
     }
@@ -20,7 +20,7 @@ class Introduction extends Component{
         for (let i=0; i<3; i++)
           {
             if (i !== idx) {
-              copied_info[idx].clicked = false;
+              copied_info[i].clicked = false;
             }
           }
         this.setState({
@@ -30,9 +30,9 @@ class Introduction extends Component{
 
     render() {return (
       <main id="main">
-        <div class="breadcrumbs">
-          <div class="container">
-            <div class="d-flex justify-content-between align-items-center">
+        <div  className="breadcrumbs">
+          <div  className="container">
+            <div  className="d-flex justify-content-between align-items-center">
               <h2>소개</h2>
               <ol>
                 <li><Link to="/">Home</Link></li>
@@ -41,30 +41,16 @@ class Introduction extends Component{
             </div>
           </div>
         </div>
-        <section class="inner-page">
-          <div class="container">
-            <div class="section-header">
+        <section  className="inner-page">
+          <div  className="container">
+            <div  className="section-header">
               <h2>학생문화공간위원회</h2>
               <p>Student Culture and Space Committee</p>
             </div>
             
-          </div>
-        </section>
-          {/* <section id="portfolio" class="portfolio">
-            <div class="container-fluid">
-
-              <div class="portfolio-isotope" data-portfolio-filter="*" data-portfolio-layout="masonry" data-portfolio-sort="original-order">
-
-                <ul class="portfolio-flters">
-                  <li data-filter="scspace-introduction">소개</li>
-                  <li data-filter="business">사업소개</li>
-                  <li data-filter="rule">회칙</li>
-                </ul>
-
-                <div class="row g-0 portfolio-container">
-                  <div class="introduction">
-                    <div data-type="scspace-introduction">
-                      <p>
+          
+        
+          {/* 
                       KAIST 학생문화공간위원회(이하 공간위)는 장영신학생회관을 비롯한 KAIST 교내 학생 문화공간의
                       총체적인 관리 및 운영을 담당하고 진정한 학생 공간을 위하여 공간에 대한 학생의 권리 보장과
                       학생 문화 발전에 기여하기 위하여 설립된 KAIST 학부 총학생회 산하 상설위원회입니다.
@@ -72,44 +58,31 @@ class Introduction extends Component{
                       공간을 관리하고 기획하는 역할을 하고 있습니다. 이를 위해, 장영신학생회관 건립 전부터 준비위원회로서
                       학생들의 의견을 수렴하고 반영해 왔으며 앞으로도 KAIST 학우 모두가 함께 문화를 만들어 갈 수 있도록
                       언제나 학우들의 이야기에 귀를 기울이고 있습니다. 저희와 같이 학생문화를 가꾸어나가고 싶다면
-                      부담 갖지 마시고 이야기를 공유해주세요. 우리 함께 장영신학생회관에서 카이스트만의 문화를 만들어봐요!
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section> */}
+                      부담 갖지 마시고 이야기를 공유해주세요. 우리 함께 장영신학생회관에서 카이스트만의 문화를 만들어봐요! */}
+          <section id="portfolio"  className="portfolio">
+              <div className="container-fluid">
+
+                <div  className="portfolio-isotope" data-portfolio-filter="*" data-portfolio-layout="masonry" data-portfolio-sort="original-order">
+
+                  <ul  className="portfolio-flters">
           {this.state.info.map((contents, idx) => {
           return (
-            <section id="portfolio" class="portfolio">
-              <div class="container-fluid">
-
-                <div class="portfolio-isotope" data-portfolio-filter="*" data-portfolio-layout="masonry" data-portfolio-sort="original-order">
-
-                  <ul class="portfolio-flters">
-                    <li data-filter="scspace-introduction">소개</li>
-                    <li data-filter="business">사업소개</li>
-                    <li data-filter="rule">회칙</li>
+              <li onClick={this.OnClickEvent.bind(this, idx)}>{contents.which}</li>
+          )
+        }
+      )}
                   </ul>
-                </div>
               </div>
-            </section>
-            
-          
-          <div className="menu-flters">
-            <h3>
-              <button className={(contents.clicked ? "" : "collapsed" )} onClick={this.OnClickEvent.bind(this, idx)} >
-                {contents.which}
-              </button>
-            </h3>
-            <div className={(contents.clicked ? "" : "collapse")}>
-              <div>
-              {contents.text}
-              </div>
-            </div>
           </div>
-      )})}
+        </section>
+  
+            <p>
+                {this.state.info.map((contents, idx) =>{
+                    if (contents.clicked === true) { return (contents.text)}  
+                })}
+             </p>
+          </div>
+        </section>
       </main>
       )};
 }
