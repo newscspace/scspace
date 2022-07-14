6c6d874e3b1f936a504508b7d7960ec4492780de
choisw0823 <choisw0823@naver.com>
2022-07-15 03:04:46 +0900

    유튜브 링크 수정

--- scspace/src/Components/Banner.js
+++ scspace/src/Components/Banner.js
@@ -10,7 +10,7 @@ class Banner extends Component{
     <p>학생문화공간위원회 홈페이지에 오신걸 환영합니다.</p>
     <div className="d-flex">
       <Link to="/reservation" className="btn-get-started scrollto">예약하기</Link>
-      <a href="https://www.youtube.com/watch?v=sW9kJ0fOKWY" className="glightbox btn-watch-video d-flex align-items-center"><i className="bi bi-play-circle"></i><span>KAIST</span></a>
+      <a href="https://www.youtube.com/watch?v=sfclu_8ZkgU" className="glightbox btn-watch-video d-flex align-items-center"><i className="bi bi-play-circle"></i><span>KAIST</span></a>
     </div>
   </div>
 </section>
