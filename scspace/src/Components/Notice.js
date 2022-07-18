fa4edb978929a4884e43b40cb4c0c80a72c973fe
DESKTOP-PKMS08J\탁건우 <towerofgod77@gmail.com>
2022-07-18 17:02:44 +0900

    건우의 첫 커밋

--- scspace/src/Components/Notice.js
+++ scspace/src/Components/Notice.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react';
 class Notice extends Component{
     render() {return (
         <div>
-            <h1>공지사항 페이지 구현</h1>
+            <h1>공지사항 페이지 구현 - 건우가 할거에요</h1>
         </div>
       )};
 }
