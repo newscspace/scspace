5e983787c37b9838fc1f4fa0f00ddcb94534b3cd
choisw0823 <choisw0823@naver.com>
2022-07-18 21:33:38 +0900

    calendar 기능

---
+++ scspace/src/Components/Calendar.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+
+const events = [{ start: new Date(), end: new Date(), title: "special event" }];
+
+const DnDCalendar = withDragAndDrop(Calendar);
+
+class App extends React.Component {
+  state = {
+    events
+  };
+
+onEventResize = (data) => {
+    const { start, end } = data;
+
+    this.setState((state) => {
+      state.events[0].start = start;
+      state.events[0].end = end;
+      return { events: state.events };
+    });
+  };
+
+  onEventDrop = (data) => {
+    console.log(data);
+  };
+
+  render() {
+    return (
+
+      <div className="App">
+        <br/><br/><br/>
+        <DnDCalendar
+          defaultDate={moment().toDate()}
+          defaultView="month"
+          events={this.state.events}
+          localizer={localizer}
+          onEventDrop={this.onEventDrop}
+          onEventResize={this.onEventResize}
+          resizable
+          style={{ height: "100vh" }}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;  
