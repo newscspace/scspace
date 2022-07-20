d293f2a77b0ce52d64ed2b356b9595803bd9b336
choisw0823 <choisw0823@naver.com>
2022-07-20 23:38:25 +0900

    디자인만 대충 해둔 공지사항

--- scspace/src/Components/Notice.js
+++ scspace/src/Components/Notice.js
@@ -1,9 +1,81 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-import NoticeList from './NoticeList';
+
 class Notice extends Component{
+
+    constructor(props){
+        super(props);
+        this.a = 0;
+        this.state = {
+            page_number : 1,
+            list : [
+                {id:1, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:2, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:3, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:4, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:5, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:1, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:2, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:3, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:4, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:5, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:1, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:2, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:3, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:4, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:5, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:2, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:3, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:4, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:5, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:1, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:2, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:3, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:4, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:5, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:2, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:3, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:4, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:5, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:1, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:2, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:3, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:4, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:5, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:2, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:3, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:4, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:5, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:1, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:2, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:3, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:4, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:5, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:2, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:3, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:4, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:5, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:1, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:2, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:3, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:4, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3},
+                {id:5, title:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", date:"2019-12-28 00:26:01", hit:3}
+
+            ]
+                
+            
+        }
+
+        
+
+    }
+
+    componentDidMount(){
+        this.setState({total_page_number : Math.ceil(this.state.list.length/10)});
+    }
+
     render() {return (
-        <main id="main">
+        <div id="main">
         <div  className="breadcrumbs">
           <div  className="container">
             <div  className="d-flex justify-content-between align-items-center">
@@ -16,26 +88,57 @@ class Notice extends Component{
           </div>
         </div>
         <section>
-          
             <div  className="section-header">
               <h2>공지사항</h2>
               <p>Notice</p>
-            
-            <hr></hr>
-            <section id="portfolio"  className="portfolio">
-              <div className="container-fluid">
+                <hr/>
+            </div>
+    <div className="container">
+        <table className="table">
+            <thead>
+               <tr>
+                <th>No</th>
+                <th>제목</th>
+                <th>날짜</th>
+                <th>조회수</th>
+               </tr>
+            </thead>
+    
+    <tbody>
+        {this.state.list.slice((this.state.page_number-1)*10, this.state.page_number*10).map((contents) => {
+            return(
+                <tr>
+                    <td>{contents.id}</td>
+                    <td>{contents.title}</td>
+                    <td>{contents.date}</td>
+                    <td>{contents.hit}</td>
+                </tr>
+            )
 
-                <div  className="portfolio-isotope" >
-                      <div>
-                        <NoticeList></NoticeList>
-                      </div>                   
-              </div>
-          </div>
-        </section>
+        })}
             
-          </div>
+    </tbody>
+</table>
+</div>
+<section className="blog">
+        <div className="blog-pagination">
+              <ul className="justify-content-center">
+                {
+                    [...Array(this.state.total_page_number)].map((v,i) => i+1).map((page_num) => {
+                        return(<li className={this.state.page_number === page_num ? "active" : ""}onClick={() => {this.setState({page_number : page_num})}}><Link to="#" >{page_num}</Link></li>)
+
+                    })
+                    
+                }
+                
+              </ul>
+            </div>
         </section>
-      </main>
+        </section>
+        
+
+
+      </div>
       )};
 }
 
