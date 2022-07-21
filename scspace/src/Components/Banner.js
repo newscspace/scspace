8ff8318b929d530c339ce4aff0a554711f2217b4
choisw0823 <choisw0823@naver.com>
2022-07-21 22:06:20 +0900

    의견좀

--- scspace/src/Components/Banner.js
+++ scspace/src/Components/Banner.js
@@ -11,6 +11,7 @@ class Banner extends Component{
     <div className="d-flex">
       <Link to="/reservation" className="btn-get-started scrollto">예약하기</Link>
       <a href="https://www.youtube.com/watch?v=sfclu_8ZkgU" className="glightbox btn-watch-video d-flex align-items-center"><i className="bi bi-play-circle"></i><span>KAIST</span></a>
+    {/*여기 뭐넣으면 좋을지 의견좀.. 안그러면 뺨때린거 넣을거야 */}
     </div>
   </div>
 </section>
