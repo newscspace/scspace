021401933b3c1035f52e746173709567d4588b19
choisw0823 <choisw0823@naver.com>
2022-08-06 17:15:48 +0900

    Notice CRUD

--- server/controllers/Notice.js
+++ server/controllers/Notice.js
@@ -1,47 +1,35 @@
+const db = require('../models/notice');
 
 notice = {
     create : (req, res) => {
-        console.log('create');
-        console.log(req.body);
+        let p = req.body;
+        p.time_post = new Date();
+        db.create(p)
+            .then (result => { result ? res.send(true) : res.send(false)});
     },
     
     read_id : (req, res) => {
-        res.json({
-                'id' : 1,
-                'title' : '더워',
-                'date' : new Date('2022-07-17T15:24:00'),
-                'hits' : 43,
-                'important' : true,
-                'content' : '여행 왔는데 폭염이라 나가지도 못하네 가이드투어 버스라도 할걸 근데 버스 해봤자 취소되더라'
-        }); // Obviously dummy code
+        db.readId([parseInt(req.query.id)])
+            .then(result => {res.json(result);})
+            .catch (() => {console.log(result);})
     },
 
     read_all : (req, res) =>{
-        res.json([
-            {
-                'id' : 1,
-                'title' : '더워',
-                'date' : new Date('2022-07-17T15:24:00'),
-                'hits' : 43,
-                'important' : true
-            },
-            {
-                'id' : 2,
-                'title' : '38도라고 미쳤어',
-                'date' : new Date('2022-07-17T16:24:00'),
-                'hits' : 4,
-                'important' : false
-            },
-        ]); // Obviously dummy code
+        db.readAll()
+            .then (result => {res.json(result);})
+            .catch (() => {console.log(result);});
     },
 
     update : (req, res) => {
-        console.log('update');
-        console.log(req.body);
+        let p = req.body;
+        p.time_edit = new Date();
+        db.update(p)
+            .then (result => { result ? res.send(true) : res.send(false)});
     }, 
 
     delete  : (req, res) => {
-        console.log('delete');
+        db.delete([parseInt(req.query.id)])
+        .then (result => { result ? res.send(true) : res.send(false)});
     }
 
 
