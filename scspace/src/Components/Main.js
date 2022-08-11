e626c0d7b477e44e5ef977fbf9c35f8dd31d7125
choisw0823 <choisw0823@naver.com>
2022-08-12 02:43:52 +0900

    class 이름 바꾸기

--- scspace/src/Components/Main.js
+++ scspace/src/Components/Main.js
@@ -2,14 +2,14 @@ import React, {Component} from 'react';
 import Banner from './Banner'
 import FastNotice from './notice/FastNotice';
 import FAQ from './faq/FAQ'
-class Header extends Component{
+class Main extends Component{
     render() {return (
         <div>
             <Banner></Banner>
             <FastNotice></FastNotice>
-            <FAQ main={true}></FAQ>
+            <FAQ main={true} history={this.props.history}></FAQ>
         </div>
       )};
 }
 
-export default Header;
+export default Main;
