0f7ec426616ab74257be96bffd3a22ea1e358b94
choisw0823 <choisw0823@naver.com>
2022-08-11 21:34:51 +0900

    db정보 .env 파일로 이동

--- server/config/db_config.js
+++ server/config/db_config.js
@@ -2,21 +2,15 @@ const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
 
-// const db_info = {
-//     host : process.env.DB_HOST,
-//     port : process.env.DB_PORT,
-//     user : process.env.DB_USER,
-//     password : process.env.DB_PWD,
-//     database : process.env.DB_NAME
-// };
-
 const db_info = {
-    host : 'localhost',
-    port : '3306',
-    user : 'root',
-    password : 'scspace',
-    database : 'scspace'
+    host : process.env.DB_HOST,
+    port : process.env.DB_PORT,
+    user : process.env.DB_USER,
+    password : process.env.DB_PWD,
+    database : process.env.DB_NAME
 };
+
+
 
 const db = {
     init: () => {
