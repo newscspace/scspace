73f75347290c740ae9f6c3c8427ff33be88410fa
choisw0823 <choisw0823@naver.com>
2022-08-14 00:29:46 +0900

    변수명 변경

--- server/routes/ask.js
+++ server/routes/ask.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const faq = require('../controllers/Ask');
+const ask = require('../controllers/Ask');
 
 
 
 
 
-router.get('/api/ask/all', faq.read_all);
+router.get('/api/ask/all', ask.read_all);
 
 router.get('/api/ask/id', ask.read_id);
 
-router.get('/api/ask/mine', faq.read_mine);
+router.get('/api/ask/mine', ask.read_mine);
 
-router.post('/api/ask/create',faq.create);
+router.post('/api/ask/create',ask.create);
 
-router.get('/api/ask/delete', faq.delete);
+router.get('/api/ask/delete', ask.delete);
 
 module.exports = {router};
 
