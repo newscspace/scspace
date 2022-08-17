d5b19dec269488480e041ff670fc224b0bebd482
choisw0823 <choisw0823@naver.com>
2022-08-18 00:40:51 +0900

    reservation 파일만 대충

--- server/controllers/Reservation.js
+++ server/controllers/Reservation.js
@@ -1,61 +1,30 @@
+const db = require('../models/notice');
 
 reservation = {
     create : (req, res) => {
-        console.log('create');
-        console.log(req.body);
+      let p = req.body;
+      p.time_request = new Date();
+      db.create(p)
+         .then (result => { result ? res.send(true) : res.send(false)});
     },
     
-    read : (req, res) => {
-        
-            res.json([
-                {
-                    space_name: 'seminar-room1',
-                    time_from: '2022-07-13T15:00:00.000Z',
-                    time_to: '2022-07-13T16:00:00.000Z',
-                    content: {
-                      organization_name: '',
-                      event_name: '',
-                      number: '4',
-                      contents: 'ㅇㅇ'
-                    },
-                    state:'granted'
-                  },
-                  {
-                    space_name: 'ullim-hall',
-                    time_from: '2022-07-18T15:00:00.000Z',
-                    time_to: '2022-07-18T16:00:00.000Z',
-                    content: {
-                      organization_name: '',
-                      event_name: '테스트용',
-                      inner_number: '5',
-                      outer_number: '2',
-                      event_purpose: 'ㄹㄹㄹㄹㄹㄹㄹ',
-                      contents: 'ㅁㅁㅁㅁㅁㅁㅁ',
-                      rehersal_from: '2022-07-17T15:00:00.000Z',
-                      rehersal_to: '2022-07-17T16:00:00.000Z',
-                      rehersal_lastday_from: '2022-07-16T15:00:00.000Z',
-                      rehersal_lastday_to: '2022-07-16T16:00:00.000Z',
-                      equipment: [ 'light' ],
-                      food: '응애',
-                      lobby: [ 'true' ],
-                      desk: '5',
-                      chair: '6',
-                      state:'wait'
-                    }
-                  }
-
-        ]); // Obviously dummy code
-        
+    read : (req, res) =>{
+     db.readAll()
+       .then (result => {res.json(result);})
+       .catch (() => {console.log(result);});
     },
 
     update : (req, res) => {
-        console.log('update');
-        console.log(req.body);
-    }, 
+      let p = req.body;
+      p.time_edit = new Date();
+      db.update(p)
+          .then (result => { result ? res.send(true) : res.send(false)});
+  }, 
 
-    delete  : (req, res) => {
-        console.log('delete');
-    }
+  delete  : (req, res) => {
+      db.delete([parseInt(req.query.id)])
+      .then (result => { result ? res.send(true) : res.send(false)});
+  }
 
 
 
---
+++ server/models/reservation.js
@@ -0,0 +1,77 @@
+const db =  require('../config/db_config');
+
+
+const dbModel = {
+  readAll: async () => {
+    let conn = db.init();
+    db.connect(conn);
+   
+    let sql = `SELECT * FROM notice;`;
+    let result = await conn.promise().query(sql)
+    .catch(err => {console.log(err); db.disconnect(conn); return null;});
+      
+    db.disconnect(conn);
+    return result[0];
+  },
+
+  readId: async (p) => {
+    let conn = db.init();
+    db.connect(conn);
+   
+    let sql = `SELECT * FROM notice where id=?;`;
+    let result = await conn.promise().query(sql, p)
+    .catch(err => {console.log(err); db.disconnect(conn); return null;});
+   
+    sql = `UPDATE notice SET hits = hits + 1 where id=?`;
+    await conn.promise().query(sql, p)
+    .catch(err => {console.log(err); db.disconnect(conn); return null;});
+   
+    db.disconnect(conn);
+    
+    return result[0][0];
+  },
+
+  create: async (p) => {
+    
+    let conn = db.init();
+    db.connect(conn);
+   
+    let sql = `INSERT INTO notice (title, time_post, important, content) VALUES (?, ?, ?, ?);`;
+    let result = await conn.promise().query(sql, [p.title, p.time_post, p.important, p.content])
+    .catch(err => {console.log(err); db.disconnect(conn); return false;});
+   
+    db.disconnect(conn);
+    
+    return true;
+  },
+
+  update: async (p) => {
+    
+    let conn = db.init();
+    db.connect(conn);
+   
+    let sql = `UPDATE notice SET title=?, time_edit=?, important=?, content=? WHERE id=?`;
+    let result = await conn.promise().query(sql, [p.title, p.time_edit, p.important, p.content, p.id])
+    .catch(err => {console.log(err); db.disconnect(conn); return false;});
+   
+    db.disconnect(conn);
+    
+    return true;
+  },
+
+  delete: async (p) => {
+    
+    let conn = db.init();
+    db.connect(conn);
+   
+    let sql = `DELETE FROM notice WHERE id=?`;
+    let result = await conn.promise().query(sql, p)
+    .catch(err => {console.log(err); db.disconnect(conn); return false;});
+   
+    db.disconnect(conn);
+    
+    return true;
+  },
+};
+
+module.exports =  dbModel;
