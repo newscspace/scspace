82e897bce330e2e98f43afb8827d8f7eb9665879
choisw0823 <choisw0823@naver.com>
2022-08-18 00:38:52 +0900

    일단 만들다 멈춘 달력

--- scspace/src/Components/Calendar.js
+++ scspace/src/Components/Calendar.js
@@ -1,53 +1,151 @@
-import React from "react";
-import { Calendar, momentLocalizer } from "react-big-calendar";
-import moment from "moment";
-import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
-import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
-import "react-big-calendar/lib/css/react-big-calendar.css";
+import React, {Component} from 'react';
+import '../static/css/calendar.css';
+import {get} from 'axios';
 
-const localizer = momentLocalizer(moment);
+class Calendar extends Component {
 
-const events = [{ start: new Date(), end: new Date(), title: "special event" }];
+  constructor(props){
+    super(props);
 
-const DnDCalendar = withDragAndDrop(Calendar);
+    this.daylist = {0:"Sun", 1:"Mon", 2:"Tue", 3:"Wed", 4:"Thu", 5:"Fri", 6:"Sat"};
+    this.monthlist = {0:"Jan", 1:"Feb", 2:"Mar", 3:"Apr", 4:"May", 5:"Jun", 6:"Jul", 7:"Aug", 8:"Sept", 9:"Oct", 10:"Nov", 11:"Dec"}
+    this.state = {
+      year : new Date().getFullYear(),
+      month : new Date().getMonth(),
+      date : new Date().getDate(),
+      day : new Date().getDay(),
+      filter : null,
+      reservation_list : [] 
+    }
 
-class App extends React.Component {
-  state = {
-    events
-  };
+    
 
-onEventResize = (data) => {
-    const { start, end } = data;
+    this.spacelist = {
+      '전체' : 'all',
+      '개인연습실':"individual-practice-room",
+      '피아노실':"piano-room",
+      '세미나실':"seminar-room",
+      '합주실':"group-practice-room",
+      '무예실':"dance-studio",
+      '울림홀':"ullim-hall",
+      '미래홀':"mirae-hall",
+      '창작공방':"workshop",
+      '오픈스페이스':"open"
+    }
+   
+  }
 
-    this.setState((state) => {
-      state.events[0].start = start;
-      state.events[0].end = end;
-      return { events: state.events };
-    });
-  };
+  MonthChangeHandler = (e) =>{
+    if (e.target.name === "prev"){
+      let new_month = this.state.month - 1;
+      if (new_month < 0) {
+        new_month = 11;
+        this.setState({month : new_month, year:this.state.year-1})
+      }
+      else{
+        this.setState({month : new_month})
+      }
+    }
+    else if (e.target.name === "next"){
+      let new_month = this.state.month + 1;
+      if (new_month > 11) {
+        new_month = 0;
+        this.setState({month : new_month, year:this.state.year+1})
+      }
+      else{
+        this.setState({month : new_month})
+      }
+    }
+    else{
+      this.setState({year : new Date().getFullYear(),
+      month : new Date().getMonth(),
+      date : new Date().getDate(),
+      day : new Date().getDay()})
+    }
+  }
 
-  onEventDrop = (data) => {
-    console.log(data);
-  };
+  SpaceHandler = (e) => {
+    this.setState({filter:e.target.value})
+  }
+componentDidMount(){
+    this.callApi()
+        .then(res => this.setState({reservation_list : res}))
+        .catch(err => console.log(err));
+}
+
+callApi= async () => {
+    const res = await get('/api/reservation/all');
+    const body = await res.data;
+    return body;
+}
+
+
 
   render() {
-    return (
+    this.firstday = (1-new Date(this.state.year+'-'+(this.state.month+1)+'-'+ '1').getDay());
+    this.lastdate =  (new Date(this.state.year, (this.state.month+1),'0').getDate());
 
-      <div className="App">
-        <br/><br/><br/>
-        <DnDCalendar
-          defaultDate={moment().toDate()}
-          defaultView="month"
-          events={this.state.events}
-          localizer={localizer}
-          onEventDrop={this.onEventDrop}
-          onEventResize={this.onEventResize}
-          resizable
-          style={{ height: "100vh" }}
-        />
-      </div>
+    return (
+        <section>
+          
+<div className="container py-5">
+<div className="calendar form-inline shadow bg-white p-5">
+
+
+    <div className="select col-lg-5">
+    <div className="col-md-4">   
+        <select className="form-control" name="team_name" onChange={this.SpaceHandler} required> 
+			      {Object.keys(this.spacelist).map((team) =>{
+              return (
+                <option key={team} value={team} >{team}</option>
+              )
+            })}
+		            </select>
+                    
+    </div>
+    </div>
+
+  <div className="d-flex align-items-center">
+    <h2 className="month font-weight-bold mb-0">{this.monthlist[this.state.month%12] + " " + this.state.year}</h2>
+    
+
+  </div>
+  
+  <div className="btn-group float-end">
+    
+    <button type="button" name="prev" className="btn btn-primary" onClick={this.MonthChangeHandler}>Prev</button>
+  
+    <button type="button" name="now" className="btn btn-light"  onClick={this.MonthChangeHandler} ></button>
+    <button type="button" name="next" className="btn btn-success" onClick={this.MonthChangeHandler}>Next</button>
+    
+  </div>
+  
+   <br/><br/><br/>
+  
+  <ol className="day dayday-names list-unstyled">
+    {Object.keys(this.daylist).map((day) => {
+        return(
+          <li className="font-weight-bold text-uppercase">{this.daylist[day]}</li>
+        )
+    })}
+  </ol>
+
+  <ol className="days list-unstyled">
+    {
+      [...Array(42)].map((n, index) => {
+       return(<li className={(this.firstday+index < 1 ||  this.firstday+index > this.lastdate) ? "outside date" : "date"}>
+                <div className={"date"}>
+                  {new Date(this.state.year, this.state.month, this.firstday+index).getDate()}
+                   
+                </div>
+              </li>)}) 
+    }
+  </ol>
+</div>
+</div>
+        </section>
     );
   }
 }
 
-export default App;  
+export default Calendar;  
