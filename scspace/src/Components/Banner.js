92e987721e94314332f58fbb9d373a9670426e68
uxhif <jiyun2176@gmail.com>
2022-08-29 00:16:21 +0900

    main page 디자인 변경

--- scspace/src/Components/Banner.js
+++ scspace/src/Components/Banner.js
@@ -1,16 +1,18 @@
 import React, {Component, useState} from 'react';
 import {Link} from 'react-router-dom';
+import {BsCalendarEventFill} from 'react-icons/bs';
 
 class Banner extends Component{
     render() {return (
       
   <section className="hero-static d-flex align-items-center">
   <div id="mount" className="container d-flex flex-column justify-content-center align-items-center text-center position-relative">
-    <h2>Welcome to <span>Scspace</span></h2>
+    <img src='./img/logo.svg' alt="" className="img-fluid main-photo"/>
+    <br/>
     <p>학생문화공간위원회 홈페이지에 오신걸 환영합니다.</p>
     <div className="d-flex">
       <Link to="/reservation" className="btn-get-started scrollto">예약하기</Link>
-      <Link to="/calendar" className="glightbox btn-watch-video d-flex align-items-center"><i className="bi bi-play-circle"></i><span>예약 현황</span></Link>
+      <Link to="/calendar" className="btn-get-started2">예약현황</Link>
     </div>
   </div>
 </section>
