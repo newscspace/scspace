cc2fe60df2718f7faec94d59744d81cbe1481d4c
choisw0823 <choisw0823@naver.com>
2022-08-31 17:17:30 +0900

    번역 수정

--- scspace/src/Components/Banner.js
+++ scspace/src/Components/Banner.js
@@ -1,22 +1,27 @@
 import React, {Component, useState} from 'react';
 import {Link} from 'react-router-dom';
-import {BsCalendarEventFill} from 'react-icons/bs';
+import {withTranslation} from "react-i18next";
 
 class Banner extends Component{
-    render() {return (
+  constructor(props){
+    super(props);
+  }
+    render() {
+      const t = this.props;
+      return (
       
   <section className="hero-static d-flex align-items-center">
   <div id="mount" className="container d-flex flex-column justify-content-center align-items-center text-center position-relative">
     <img src='./img/logo.svg' alt="" className="img-fluid main-photo"/>
     <br/>
-    <p>학생문화공간위원회 홈페이지에 오신걸 환영합니다.</p>
+    <p>{t('학생문화공간위원회 홈페이지에 오신걸 환영합니다.')}</p>
     <div className="d-flex">
-      <Link to="/reservation" className="btn-get-started scrollto">예약하기</Link>
-      <Link to="/calendar" className="btn-get-started2">예약현황</Link>
+      <Link to="/reservation" className="btn-get-started scrollto">{t('예약하기')}</Link>
+      <Link to="/calendar" className="btn-get-started2">{t('예약현황')}</Link>
     </div>
   </div>
 </section>
       )};
 }
 
-export default Banner;
+export default withTranslatoin()(Banner);
