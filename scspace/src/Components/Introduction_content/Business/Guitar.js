edfa5ab33ed417bb025f931e8e59ef639110d93f
DreamSailed <swr2030301@gmail.com>
2022-11-23 01:14:41 +0900

    오타수정

--- scspace/src/Components/Introduction_content/Business/Guitar.js
+++ scspace/src/Components/Introduction_content/Business/Guitar.js
@@ -10,7 +10,7 @@ class Guitar extends Component{
                 학생문화공간위원회에서 하는 업무들은 아래와 같습니다.
                 </b></p>
                 <ul>
-                    <li><i className="bi bi-check-circle-fill"></i><b>상시 업무</b><br/><p className="in">사무실 상근, 분실문 관리, 합주실 팀 관리, CCTV열람 관리</p></li>
+                    <li><i className="bi bi-check-circle-fill"></i><b>상시 업무</b><br/><p className="in">사무실 상근, 분실물 관리, 합주실 팀 관리, CCTV열람 관리</p></li>
                     <li><i className="bi bi-check-circle-fill"></i><b>정기 업무</b><br/><p className="in">신학관 반상회</p></li>
                     <li><i className="bi bi-check-circle-fill"></i><b>비정기 업무</b><br/><p className="in">모임터 사석화 방지</p></li>
                 </ul>
