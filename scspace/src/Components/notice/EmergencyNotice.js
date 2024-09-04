4ab99b400476afb0a457a3c87426ae4edcbd15d4
Gerbera3090 <hw3090@gmail.com>
2024-09-04 10:13:48 +0900

    fix: notice changed

--- scspace/src/Components/notice/EmergencyNotice.js
+++ scspace/src/Components/notice/EmergencyNotice.js
@@ -3,12 +3,9 @@ import React from "react";
 const EmergencyNotice = () => {
   return (
     <div class="emernotice-container">
-      <p>
-        {" "}
-        사이트 리뉴얼을 위해, 8.30 ~ 9.5 중에 사이트 이용 장애가 발생하거나 예약
-        누락이 발생할 수 있습니다.{" "}
-      </p>
-      <p> 학우분들의 양해 부탁드립니다. </p>
+      <p>사이트 장애 발생으로 인해, 복구 작업 중입니다...</p>
+      <p> 9/8(일)까지 처리하겠습니다. </p>
+      <p> 불편을 드림에 사과드립니다.</p>
     </div>
   );
 };
