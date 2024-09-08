620b7178e535ccd986f4abd2132e6b330d296232
Gerbera3090 <hw3090@gmail.com>
2024-09-09 08:05:41 +0900

    Fix day extended

--- scspace/src/Components/notice/EmergencyNotice.js
+++ scspace/src/Components/notice/EmergencyNotice.js
@@ -4,7 +4,7 @@ const EmergencyNotice = () => {
   return (
     <div class="emernotice-container">
       <p>사이트 장애 발생으로 인해, 복구 작업 중입니다...</p>
-      <p> 9/8(일)까지 처리하겠습니다. </p>
+      <p> 9/11(수)까지 처리하겠습니다. </p>
       <p> 불편을 드림에 사과드립니다.</p>
       <p>
         {" "}
